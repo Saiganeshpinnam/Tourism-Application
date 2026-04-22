213554b057daab03141304aa16d80bd10527e2d0
Ganesh <saiganeshpinnam41@gmail.com>
2026-04-22 10:40:48 +0530

    final first solution before deploying

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,11 +1,31 @@
 import React from "react";
-import AppNavigator from "./src/navigation/AppNavigator";
+
 import { FavoritesProvider } from "./src/context/FavoritesContext";
-
-
+import { useEffect } from "react";
+import * as ScreenCapture from "expo-screen-capture";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
-  return (<FavoritesProvider>
-  <AppNavigator />
-</FavoritesProvider>)
+
+  useEffect(() => {
+    ScreenCapture.preventScreenCaptureAsync();
+
+    return () => {
+      ScreenCapture.allowScreenCaptureAsync();
+    };
+  }, []);
+
+  return  (<FavoritesProvider>
+   <AppNavigator />
+ </FavoritesProvider>)
 }
+
+
+
+// allows screenshots
+
+// export default function App() {
+//   return (<FavoritesProvider>
+//   <AppNavigator />
+// </FavoritesProvider>)
+// }
